74e070eb541975fe014cd3bf970c009d50eb8b33
Lucas Picasarri-Arrieta <lucaspicasarri@gmail.com>
2022-06-02 12:14:51 +0200

    first commit

---
+++ TpMock/src/test/java/org/uca/todomanager/TestTodoManager.java
@@ -0,0 +1,20 @@
+package org.uca.todomanager;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+
+public class TestTodoManager {
+
+    @Test
+    public void test(){
+        fail("not implemented");
+    }
+}
